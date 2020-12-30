e188660daadbe0d1729b9442c3b9a385ba0c73e1
グエン　ナム <ngocnam.sk@gmail.com>
2020-12-31 01:43:36 +0900

    Vuetify

---
+++ frontend/enduser/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <SideBar/>
+    <Header/>
+
+    <!-- Sizes your content based upon application components -->
+    <MainFrame/>
+
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import Vue from 'vue';
+import vuetify from '@/plugins/vuetify';
+
+import Header from './layout/Header.vue';
+import Footer from './layout/Footer.vue';
+import MainFrame from './layout/MainFrame.vue';
+import SideBar from './layout/SideBar.vue';
+// import vuetify from '@/plugins/vuetify'
+
+Vue.use(vuetify);
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    MainFrame,
+    SideBar,
+    // vuetify,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/enduser/src/main.js
@@ -0,0 +1,17 @@
+// import Vue from 'vue';
+import { createApp } from 'vue';
+
+// import Vuetify from 'vuetify';
+// import 'vuetify/dist/vuetify.min.css';
+
+// import vuetify from '@/plugins/vuetify'
+
+import App from './App.vue';
+
+// Vue.use(Vuetify);
+
+// const opts = {}
+
+// export default new Vuetify(opts)
+
+createApp(App).mount('#app');
